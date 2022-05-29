fca021ab62c775b34927b841986f282e4ebfcc91
eko <ekoptk@gmail.com>
2022-05-29 11:20:36 +0700

    Update header.js
    add pistore logo

--- components/Header.js
+++ components/Header.js
@@ -1,8 +1,16 @@
-
+import Image from 'next/image'
 
 function Header() {
   return (
     <header className="flex flex-col m-5 justify-between items-center">
+      <Image
+          src="/pistore-logo.png"
+          alt="Logo PiSTORE"
+          className='flex flex-row'
+          width={150}
+          height={150}
+          priority
+        />
       <h1 className="text-3xl font-bold">
       <span className='text-yellow-400'>Pi </span>STORE
       </h1>
